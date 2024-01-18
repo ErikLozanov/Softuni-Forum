d82dbfd1613eb0a0cc181a8e26f09b415e7c5be7
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2024-01-18 23:35:41 +0200

    added themes component + subscribed to getThemes service

---
+++ client/src/app/themes-list/themes-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Theme } from '../types/theme';
+
+@Component({
+  selector: 'app-themes-list',
+  templateUrl: './themes-list.component.html',
+  styleUrl: './themes-list.component.css'
+})
+
+export class ThemesListComponent implements OnInit {
+  themesList: Theme[] = [];
+  isLoading: boolean = true;
+
+  constructor(private apiService: ApiService) {};
+
+  ngOnInit(): void {
+
+    //Just like then: this.apiService.getThemes().subscribe((themes) => {
+    this.apiService.getThemes().subscribe({
+      next: (themes) => {
+        this.themesList = themes;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.log(err);
+      }
+          
+    });
+  }
+}
