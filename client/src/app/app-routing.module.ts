7253a96005d554dddaa63dfe2cc40e51412685ae
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2024-02-02 23:42:30 +0200

    Update app-routing.module.ts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NewThemeComponent } from './theme/new-theme/new-theme.component';
+import { AuthActivate } from './core/guards/auth.activate';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent},
+  { path: 'add-theme', component: NewThemeComponent, canActivate: [AuthActivate]}
 ];
 
 @NgModule({
