6a0feb35f4c71c8def5907fcce13de2b81e059fb
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2024-02-02 23:42:48 +0200

    Update current-theme.component.ts

--- client/src/app/theme/current-theme/current-theme.component.ts
+++ client/src/app/theme/current-theme/current-theme.component.ts
@@ -2,25 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { Theme } from '../../types/theme';
 import { ApiService } from '../../api.service';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-current-theme',
   templateUrl: './current-theme.component.html',
-  styleUrl: './current-theme.component.css'
+  styleUrl: './current-theme.component.css',
 })
 export class CurrentThemeComponent implements OnInit {
   theme: Theme | undefined;
 
-  constructor(private apiService: ApiService, private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private apiService: ApiService,
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService
+  ) {}
+
+  get isLogged(): boolean {
+    return this.userService.isLogged;
+  }
 
   ngOnInit(): void {
     this.fetchTheme();
   }
 
-  fetchTheme():void {
+  fetchTheme(): void {
     const id = this.activatedRoute.snapshot.params['themeId'];
     this.apiService.getTheme(id).subscribe((newTheme) => {
       this.theme = newTheme;
+      console.log(this.theme);
     });
   }
 }
