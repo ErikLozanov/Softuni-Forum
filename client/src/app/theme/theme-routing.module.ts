d72a327a7dbd47575d6e9bb0657c599d318774ec
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2024-02-05 22:38:06 +0200

    Update theme-routing.module.ts

--- client/src/app/theme/theme-routing.module.ts
+++ client/src/app/theme/theme-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from '../main/main.component';
-import { NewThemeComponent } from './new-theme/new-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
 import { AuthActivate } from '../core/guards/auth.activate';
 
@@ -12,11 +11,6 @@ const routes: Routes = [
       { path: '', pathMatch: 'full', component: MainComponent },
       { path: ':themeId', component: CurrentThemeComponent },
     ],
-  },
-  {
-    path: 'add-theme',
-    component: NewThemeComponent,
-    canActivate: [AuthActivate],
   },
 ];
 
